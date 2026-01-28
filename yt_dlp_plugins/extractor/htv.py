0d1207bebe6d7bd7e1c6416856997d6f88332e45
Cynthia <cynthia2048@proton.me>
2026-01-28 21:41:02 +0530

    More robust metadata parsing for hanime.tv

--- yt_dlp_plugins/extractor/htv.py
+++ yt_dlp_plugins/extractor/htv.py
@@ -1,7 +1,8 @@
+import json
 import subprocess
 
 from yt_dlp.extractor.common import InfoExtractor
-from yt_dlp.utils import str_or_none, int_or_none
+from yt_dlp.utils import str_or_none, int_or_none, ExtractorError
 # TODO Allow any runtime not just Deno.
 from yt_dlp.utils._jsruntime import DenoJsRuntime
 
@@ -17,7 +18,7 @@ class HanimeTVIE(InfoExtractor):
     }, {
         set(o, k, v) {
             if (k == "ssignature" || k == "stime")
-                console.log(v);
+                console.log(k, v);
             
             o[k] = v;
             return true;
@@ -31,43 +32,59 @@ class HanimeTVIE(InfoExtractor):
 
     def __init__(self):
         self._runtime = DenoJsRuntime()
+
+        if not self._runtime.info:
+            raise ExtractorError("DenoJS is required for hanime.tv extractor")
+        
         self._script = None
 
-    def _cache_program(self, url, video_id):
+    def _cache_credential_generator(self, url, video_id):
         self._script = self._JS_PREAMBLE
         self._script += self._download_webpage(url, video_id,
             headers={'Referer': 'https://hanime.tv/'}, note='Caching generator script')
 
-    def _generate_creds(self):
-        info = self._runtime.info
-        output = subprocess.run([info.path, 'run', '-'],
+    def _generate_credentials(self):
+        output = subprocess.run([self._runtime.info.path, 'run', '-'],
             input=self._script, text=True, capture_output=True)
 
         if output.returncode == 0:
-            return output.stdout.split('\n', 1)
+            creds = dict(line.split(' ', 1)
+                for line in output.stdout.split('\n', 1))
+
+            return creds.get('ssignature'), creds.get('stime')
         else:
-            return None
+            raise ExtractorError("Signature and timestamp generation failed")
+
+    def _js_to_json(self, meta):
+        output = subprocess.run([self._runtime.info.path, 'run', '-'],
+                                input=f"console.log(JSON.stringify({meta}.state.data.video.hentai_video))",
+                                text=True, capture_output=True)
+
+        if output.returncode == 0:
+            return json.loads(output.stdout)
+        else:
+            raise ExtractorError("Metadata extraction failed")
 
     def _real_extract(self, url):
-        slug = self._match_id(url)
-
-        page = self._download_webpage(url, slug)
-        title = self._search_regex(r'<h1 class="tv-title">([^<]+)', page, "title")
-        video_id = self._search_regex(rf'(\d+)\s*,\s*"{slug}"', page, "video id")
+        video_id = self._match_id(url)
+        page = self._download_webpage(url, video_id)
 
         # NOTE This script is unlikely to change, so better cache it.
         if not self._script:
             script_url = self._search_regex( 
                 r'<script.*src="(https://hanime-cdn\.com/js/vendor\.[^"]+)', page, "signature generator"
             )
-            self._cache_program(script_url, slug)
+            self._cache_credential_generator(script_url, video_id)
 
-        ssignature, stime = self._generate_creds()
-        self.to_screen(f'Signature: {ssignature}')
+        js_metadata = self._search_regex(r'<script>window.__NUXT__\s*=\s*(.*?);</script>', page, "metadata")
+        video_info = self._js_to_json(js_metadata)
+        
+        ssignature, stime = self._generate_credentials()
+        # self.to_screen(f'Signature: {ssignature}')
 
         manifest = self._download_json(
-            f'https://cached.freeanimehentai.net/api/v8/guest/videos/{video_id}/manifest',
-            slug, headers={
+            f"https://h.freeanimehentai.net/api/v8/guest/videos/{video_info['id']}/manifest",
+            video_id, headers={
                 'Accept': 'application/json',
                 'Origin': 'https://hanime.tv',
                 'Referer': 'https://hanime.tv/',
@@ -78,8 +95,8 @@ class HanimeTVIE(InfoExtractor):
         )
 
         formats = []
-        servers = manifest['videos_manifest']['servers']
-        for server in servers:
+
+        for server in manifest['videos_manifest']['servers']:
             for stream in server['streams']:
                 formats.append({
                     'url': stream['url'],
@@ -89,9 +106,9 @@ class HanimeTVIE(InfoExtractor):
                     'height': int_or_none(stream.get('height')),
                     'filesize_approx': int_or_none(stream.get('filesize_mbs'), invscale=1000000),
                 })
-
+        
         return {
-            'id': slug,
-            'title': title,
+            'id': video_id,
+            'title': video_info['name'],
             'formats': formats
         }
